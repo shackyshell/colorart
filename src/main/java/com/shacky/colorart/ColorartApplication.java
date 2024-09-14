af568fcec83bec115b45e6ad0db8297a3b994e0b
Julia Zajusz <17937394+shackyshell@users.noreply.github.com>
2024-09-14 13:53:39 +0200

    spring initialzr

---
+++ src/main/java/com/shacky/colorart/ColorartApplication.java
@@ -0,0 +1,13 @@
+package com.shacky.colorart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shacky/colorart/ColorartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shacky.colorart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
